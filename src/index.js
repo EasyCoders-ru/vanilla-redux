2236265b505b2fe8c9e0123e5c2f0594d85291d7
Anton Golosnichenko <golosnichenko@artics.ru>
2021-04-11 14:30:22 +0300

    1.2 Экшены

--- src/index.js
+++ src/index.js
@@ -4,10 +4,29 @@ const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+countStore.dispatch({ type: "MINUS" });
+countStore.dispatch({ type: "MINUS" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
