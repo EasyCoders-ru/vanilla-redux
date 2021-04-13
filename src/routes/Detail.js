52eed57e4316cb8648b08a2d4ca96019399ab178
Anton Golosnichenko <golosnichenko@artics.ru>
2021-04-13 03:34:31 +0300

    3.5 Страница задачи

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,22 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+
+function Detail({ toDo }) {
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <h5>Задача создана в: {toDo?.id}</h5>
+    </>
+  );
+}
+
+function mapStateToProps(state, ownParams) {
+  const {
+    match: {
+      params: { id },
+    },
+  } = ownParams;
+  return { toDo: state.find((toDo) => toDo.id === parseInt(id)) };
+}
+
+export default connect(mapStateToProps)(Detail);
